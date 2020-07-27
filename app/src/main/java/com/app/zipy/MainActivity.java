98fb838635ec9f4deaff7967fc158479aba2a155
Kizilov <kizilov.yura@gmail.com>
2020-07-27 15:44:01 +0300

    fix analytic params

--- app/src/main/java/com/app/zipy/MainActivity.java
+++ app/src/main/java/com/app/zipy/MainActivity.java
@@ -285,16 +285,17 @@ public class MainActivity extends Activity implements OSSubscriptionObserver {
     }
 
     private String addParamsToURL(String url) {
-        if (url.contains("utm_medium=app&utm_source=app") || url.contains("utm_medium%3Dapp%26utm_source%3Dapp") || !url.contains("zipy.co.il")) {
+
+        if (url.contains("utm_medium=app&utm_source=app_android") || url.contains("utm_medium%3Dapp%26utm_source%3Dapp_android") || !url.contains("zipy.co.il")) {
             return url;
         }
 
         if (url.contains("#")) {
             String[] urlArray = url.split("#");
-            String params = url.contains("?") ? "&utm_medium=app&utm_source=app" : "?utm_medium=app&utm_source=app";
+            String params = url.contains("?") ? "&utm_medium=app&utm_source=app_android" : "?utm_medium=app&utm_source=app_android";
             return urlArray[0] + params + "#" + urlArray[1];
         } else {
-            return url += url.contains("?") ? "&utm_medium=app&utm_source=app" : "?utm_medium=app&utm_source=app";
+            return url += url.contains("?") ? "&utm_medium=app&utm_source=app_android" : "?utm_medium=app&utm_source=app_android";
         }
     }
 
