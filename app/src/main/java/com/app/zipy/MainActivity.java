0b0d565f8867b77237533112c466dd097617a347
Kizilov <kizilov.yura@gmail.com>
2020-11-23 14:25:13 +0300

    fixed activity reload after orientation change and upgrade version

--- app/src/main/java/com/app/zipy/MainActivity.java
+++ app/src/main/java/com/app/zipy/MainActivity.java
@@ -73,6 +73,7 @@ public class MainActivity extends Activity implements OSSubscriptionObserver {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
